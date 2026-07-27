ea69268d0fd5546aa9a8641a071761f9b75a0e2f
gustavoZuin237 <zuingustavo206@gmail.com>
2026-07-27 11:29:50 -0300

    Styling tweaks: made styling adjustments to the FileName modal for responsiveness and corrected weird overlay positioning

--- src/components/FileNameDialog/styles.ts
+++ src/components/FileNameDialog/styles.ts
@@ -2,19 +2,31 @@ import styled from "styled-components";
 
 export const FilenameDialogContent = styled.div`
   border-radius: 12px;
-  width: 50%;
+  max-width: 40rem;
+  margin-inline: auto;
   display: flex;
   flex-direction: column;
-  justify-self: center;
   align-items: center;
   padding: 2rem 4rem;
   gap: 2rem;
   background-color: ${(props) => props.theme["surface"]};
   border: 1px solid ${({ theme }) => theme["primarySoft"]};
+  text-align: center;
+
+  @media (max-width: 480px) {
+    font-size: 0.65rem;
+  }
 `;
 
 export const ButtonContainer = styled.div`
+  width: 100%;
   display: flex;
   justify-content: center;
+  align-items: center;
   gap: 1rem;
+
+  @media (max-width: 580px) {
+    flex-direction: column;
+    gap: 0.5rem;
+  }
 `;
